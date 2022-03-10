21e7a2657c581a9e03b754514f3ff088e05e518b
nabilk11 <khan.nabiln11@gmail.com>
2022-03-09 21:42:02 -0500

    installed express and mysql to server

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { AddEmployee } from './components/AddEmployee/AddEmployee';
+
+function App() {
+  return (
+    <div className="App">
+      <AddEmployee />
+    </div>
+  );
+}
+
+export default App;
