97ef20ee49cea28b57fd6ed4af5be85f891db42d
nabilk11 <khan.nabiln11@gmail.com>
2022-03-09 22:27:25 -0500

    added employeeSystem.employees table to mysql workbench, added post route for employee

--- server/server.js
+++ server/server.js
@@ -1,10 +1,41 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql')
 const PORT = process.env.PORT || 3001;
 
+// MY SQL DB
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: '',
+    database: 'employeeSystem',
+})
+
+// ROUTES & CONTROLLERS
+
+app.post('/create', (req, res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+    const country = req.body.country;
+    const position = req.body.position;
+    const wage = req.body.wage;
+
+db.query(
+    'INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)',
+     [name, age, country, position, wage], 
+     (err, result) => {
+         if (err) {
+             console.log(err)
+         } else {
+             res.send("Values Inserted")
+         }
+     }
+     )
+})
 
 
 
+// LISTENER
 app.listen(PORT, ()=> {
     console.log("Server is running on Port: "+PORT)
 })
